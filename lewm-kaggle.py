1c6946010f0278bddbef02f98e76b00b51fdbe3f
Luke Barbier <lukebarbier12@gmail.com>
2026-08-16 08:45:08 -0500

    git init

---
+++ lewm-kaggle.py
@@ -0,0 +1,273 @@
+import os
+from pathlib import Path
+
+import hydra
+import lightning as pl
+import stable_pretraining as spt
+from stable_pretraining import data as dt
+import stable_worldmodel as swm
+import torch
+from lightning.pytorch.loggers import WandbLogger
+from omegaconf import OmegaConf, open_dict
+
+from functools import partial
+from stable_worldmodel.data import column_normalizer as get_column_normalizer
+from stable_worldmodel.wm.loss import SIGReg
+from lightning.pytorch.callbacks import Callback
+from stable_worldmodel.wm.utils import save_pretrained
+
+
+def get_img_preprocessor(source: str, target: str, img_size: int = 224):
+    imagenet_stats = dt.dataset_stats.ImageNet
+    to_image = dt.transforms.ToImage(
+        **imagenet_stats, source=source, target=target
+    )
+    resize = dt.transforms.Resize(img_size, source=source, target=target)
+    return dt.transforms.Compose(to_image, resize)
+
+
+class SaveCkptCallback(Callback):
+    """Callback to save model checkpoint after each epoch using save_pretrained."""
+
+    def __init__(self, run_name, cfg, epoch_interval: int = 1, hf_cfg=None):
+        super().__init__()
+        self.run_name = run_name
+        self.cfg = cfg
+        self.epoch_interval = epoch_interval
+        self.hf_cfg = hf_cfg or {}
+        self._hf_api = None
+
+    def on_sanity_check_end(self, trainer, pl_module):
+        super().on_sanity_check_end(trainer, pl_module)
+        if trainer.is_global_zero and self.hf_cfg.get('enabled'):
+            self._save(pl_module.model, 0)
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        super().on_train_epoch_end(trainer, pl_module)
+
+        if trainer.is_global_zero:
+            if (trainer.current_epoch + 1) % self.epoch_interval == 0:
+                self._save(pl_module.model, trainer.current_epoch + 1)
+
+            # save final epoch
+            if (trainer.current_epoch + 1) == trainer.max_epochs:
+                self._save(pl_module.model, trainer.current_epoch + 1)
+
+    def _get_hf_api(self):
+        if self._hf_api is not None:
+            return self._hf_api
+
+        import logging
+        from huggingface_hub import HfApi
+        from huggingface_hub.utils import disable_progress_bars
+
+        disable_progress_bars()
+        logging.getLogger('huggingface_hub').setLevel(logging.ERROR)
+        from kaggle_secrets import UserSecretsClient
+        
+        token = UserSecretsClient().get_secret(
+            self.hf_cfg.get('token_secret', 'HF_TOKEN')
+        )
+        # token = self.hf_cfg.get('token_secret') # just for running locally
+        self._hf_api = HfApi(token=token)
+        return self._hf_api
+
+    def _upload_to_hf(self, weights_file):
+        if not self.hf_cfg.get('enabled'):
+            return
+
+        repo_id = self.hf_cfg['repo_id']
+        ckpt_dir = swm.data.utils.get_cache_dir(sub_folder='checkpoints') / self.run_name
+        prefix = self.hf_cfg.get('path_prefix') or ''
+        if prefix and not prefix.endswith('/'):
+            prefix = f'{prefix}/'
+
+        hf_api = self._get_hf_api()
+        hf_api.upload_file(
+            path_or_fileobj=str(ckpt_dir / weights_file),
+            path_in_repo=f'{prefix}{weights_file}',
+            repo_id=repo_id,
+            repo_type='model',
+        )
+        config_path = ckpt_dir / 'config.json'
+        if config_path.exists():
+            hf_api.upload_file(
+                path_or_fileobj=str(config_path),
+                path_in_repo=f'{prefix}config.json',
+                repo_id=repo_id,
+                repo_type='model',
+            )
+        print(f'Uploaded {weights_file} to {repo_id}')
+
+    def _save(self, model, epoch):
+        filename = f'weights_epoch_{epoch}.pt'
+        save_pretrained(
+            model,
+            run_name=self.run_name,
+            config=self.cfg,
+            filename=filename,
+        )
+        self._upload_to_hf(filename)
+
+
+def lejepa_forward(self, batch, stage, cfg):
+    """encode observations, predict next states, compute losses."""
+
+    ctx_len = cfg.wm.history_size
+    n_preds = cfg.wm.num_preds
+    lambd = cfg.loss.sigreg.weight
+
+    # Replace NaN values with 0 (occurs at sequence boundaries)
+    batch['action'] = torch.nan_to_num(batch['action'], 0.0)
+
+    output = self.model.encode(batch)
+
+    emb = output['emb']  # (B, T, D)
+    act_emb = output['act_emb']
+
+    ctx_emb = emb[:, :ctx_len]
+    ctx_act = act_emb[:, :ctx_len]
+
+    tgt_emb = emb[:, n_preds:]  # label
+    pred_emb = self.model.predict(ctx_emb, ctx_act)  # pred
+
+    # LeWM loss
+    output['pred_loss'] = (pred_emb - tgt_emb).pow(2).mean()
+    output['sigreg_loss'] = self.sigreg(emb.transpose(0, 1))
+    output['loss'] = output['pred_loss'] + lambd * output['sigreg_loss']
+
+    losses_dict = {
+        f'{stage}/{k}': v.detach() for k, v in output.items() if 'loss' in k
+    }
+    self.log_dict(losses_dict, on_step=True, sync_dist=True)
+    return output
+
+
+@hydra.main(version_base=None, config_path='./config', config_name='lewm')
+def run(cfg):
+    #########################
+    ##       dataset       ##
+    #########################
+
+    dataset_cfg = OmegaConf.to_container(cfg.data.dataset, resolve=True)
+    dataset_name = dataset_cfg.pop('name')
+    cache_dir = os.environ.get('LOCAL_DATASET_DIR', None)
+    print(
+        f'Loading dataset "{dataset_name}" from {"local cache: " + cache_dir if cache_dir else "default location"}'
+    )
+    dataset = swm.data.load_dataset(
+        dataset_name, transform=None, cache_dir=cache_dir, **dataset_cfg
+    )
+    transforms = [
+        get_img_preprocessor(
+            source='pixels', target='pixels', img_size=cfg.img_size
+        )
+    ]
+
+    with open_dict(cfg):
+        for col in cfg.data.dataset.keys_to_load:
+            if col.startswith('pixels'):
+                continue
+
+            normalizer = get_column_normalizer(dataset, col, col)
+            transforms.append(normalizer)
+
+        cfg.model.action_encoder.input_dim = (
+            cfg.data.dataset.frameskip * dataset.get_dim('action')
+        )
+
+    transform = spt.data.transforms.Compose(*transforms)
+    dataset.transform = transform
+
+    rnd_gen = torch.Generator().manual_seed(cfg.seed)
+    train_set, val_set = spt.data.random_split(
+        dataset,
+        lengths=[cfg.train_split, 1 - cfg.train_split],
+        generator=rnd_gen,
+    )
+
+    train = torch.utils.data.DataLoader(
+        train_set,
+        **cfg.loader,
+        generator=rnd_gen,
+    )
+    val_cfg = {**cfg.loader}
+    val_cfg['shuffle'] = False
+    val_cfg['drop_last'] = False
+    val = torch.utils.data.DataLoader(val_set, **val_cfg)
+
+    ##############################
+    ##       model / optim      ##
+    ##############################
+
+    world_model = hydra.utils.instantiate(cfg.model)
+
+    total_steps = cfg.trainer.max_epochs * len(train)
+    optimizers = {
+        'model_opt': {
+            'modules': 'model',
+            'optimizer': dict(cfg.optimizer),
+            'scheduler': {
+                'type': 'LinearWarmupCosineAnnealingLR',
+                'warmup_steps': max(1, int(0.01 * total_steps)),
+                'max_steps': total_steps,
+            },
+            'interval': 'epoch',
+        },
+    }
+
+    data_module = spt.data.DataModule(train=train, val=val)
+    world_model = spt.Module(
+        model=world_model,
+        sigreg=SIGReg(**cfg.loss.sigreg.kwargs),
+        forward=partial(lejepa_forward, cfg=cfg),
+        optim=optimizers,
+    )
+
+    ##########################
+    ##       training       ##
+    ##########################
+
+    run_id = cfg.get('subdir') or ''
+    run_dir = Path(
+        swm.data.utils.get_cache_dir(sub_folder='checkpoints'), run_id
+    )
+
+    logger = None
+    if cfg.wandb.enabled:
+        logger = WandbLogger(**cfg.wandb.config)
+        logger.log_hyperparams(OmegaConf.to_container(cfg))
+
+    run_dir.mkdir(parents=True, exist_ok=True)
+    with open(run_dir / 'config.yaml', 'w') as f:
+        OmegaConf.save(cfg, f)
+
+    save_ckpt_callback = SaveCkptCallback(
+        run_name=cfg.output_model_name,
+        cfg=cfg.model,
+        epoch_interval=1,
+        hf_cfg=cfg.get('hf'),
+    )
+
+    trainer = pl.Trainer(
+        **cfg.trainer,
+        callbacks=[save_ckpt_callback],
+        num_sanity_val_steps=1,
+        logger=logger,
+        enable_checkpointing=True,
+    )
+
+    ckpt_path = run_dir / f'{cfg.output_model_name}_weights.ckpt'
+    manager = spt.Manager(
+        trainer=trainer,
+        module=world_model,
+        data=data_module,
+        ckpt_path=ckpt_path if ckpt_path.exists() else None,
+    )
+
+    manager()
+    return
+
+
+if __name__ == '__main__':
+    run()
