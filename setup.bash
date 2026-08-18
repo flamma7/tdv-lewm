3a2d377a18158ccf590a6b29856bcb3b64d707dd
Luke Barbier <lukebarbier12@gmail.com>
2026-08-18 15:16:59 -0500

    saving setup.bash

---
+++ setup.bash
@@ -0,0 +1,5 @@
+export STABLEWM_HOME=/workspace
+pip install 'stable-worldmodel[train]==0.1.1' transformers==4.57.1 imageio ale-py
+pip freeze |grep stable
+pip freeze |grep transformers
+wandb login --verify
