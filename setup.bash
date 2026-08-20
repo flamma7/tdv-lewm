21791949beda060866022ca72e3fa474284d8e5a
Luke Barbier <lukebarbier12@gmail.com>
2026-08-19 20:37:36 -0500

    faster setup

--- setup.bash
+++ setup.bash
@@ -1,3 +1,11 @@
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 HF_TOKEN WANDB_API_KEY" >&2
+  exit 1
+fi
+
+export HF_TOKEN="$1"
+export WANDB_API_KEY="$2"
+
 export STABLEWM_HOME=/workspace
 HF_DATASET=galilai-group/lewm-pusht
 HF_DATASET_DIR=galilai-group--lewm-pusht
@@ -7,6 +15,6 @@ pip freeze |grep stable
 pip freeze |grep transformers
 
 wandb login --verify
-hf auth login
+hf auth whoami
 
 hf download ${HF_DATASET} --repo-type dataset --local-dir ${STABLEWM_HOME}/datasets/${HF_DATASET_DIR}/
