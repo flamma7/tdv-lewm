5a25eced786b25d6d059c3c58d9f7492a23c65ba
Luke Barbier <lukebarbier12@gmail.com>
2026-08-20 21:34:57 -0500

    ready for go time!

--- run.sh
+++ run.sh
@@ -2,20 +2,18 @@
 
 shutdown="${1:-false}"
 
-# TODO revert max_epochs and wandb disabled
-
 python lewm.py \
   -cn lewm.yaml \
   output_model_name=lewm_lr1.225e-4_bs256_gpu3 \
   data.dataset.name=galilai-group/ogb_cube_single \
-  wandb.enabled=false \
+  wandb.enabled=true \
   hf.enabled=true \
   data=ogb \
-  trainer.max_epochs=2
+  trainer.max_epochs=10
 status=$?
 
 if [[ "${shutdown,,}" == "true" ]]; then
-  runpodctl pod stop "$RUNPOD_POD_ID"
+  runpodctl stop pod "$RUNPOD_POD_ID"
 fi
 
 exit "$status"
