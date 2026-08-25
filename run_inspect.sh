a6c8b8a4029a37cb93d3d4a0bcc1c599f1646e42
Luke Barbier <lukebarbier12@gmail.com>
2026-08-24 19:36:03 -0500

    test inspection

---
+++ run_inspect.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+shutdown="${1:-false}"
+lr="${2:-5e-5}"
+alpha="${3:-0.1}"
+lamb="${4:-1}"
+
+if [[ "$(basename "$PWD")" != "stable-worldmodel" ]]; then
+  if [[ -d "${PWD}/stable-worldmodel" ]]; then
+    cd "${PWD}/stable-worldmodel"
+  else
+    echo "Error: cwd is not stable-worldmodel and no stable-worldmodel/ directory in ${PWD}" >&2
+    exit 1
+  fi
+fi
+
+python scripts/train/lewm_inspect.py \
+  output_model_name=quentinll-lewm-cube \
+  data.dataset.name=galilai-group/ogb_cube_single \
+  wandb.enabled=true \
+  hf.enabled=false \
+  data=ogb \
+  trainer.max_epochs=10 \
+  num_workers=6 \
+
+status=$?
+
+if [[ "${shutdown,,}" == "true" ]]; then
+  runpodctl stop pod "$RUNPOD_POD_ID"
+fi
+
+exit "$status"
