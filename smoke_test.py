e403b61df2043bd58d8fcdf92f26ad0e7845880e
Luke Barbier <lukebarbier12@gmail.com>
2026-08-26 17:35:56 -0500

    test an error..

--- smoke_test.py
+++ smoke_test.py
@@ -1,3 +1,9 @@
-import torch
-print(torch.cuda.get_device_name(0))
-print(torch.zeros(1, device="cuda"))
+# import torch
+# print(torch.cuda.get_device_name(0))
+# print(torch.zeros(1, device="cuda"))
+
+raise RuntimeError(
+    "CUDA unknown error - this may be due to an incorrectly set up environment, "
+    "e.g. changing env variable CUDA_VISIBLE_DEVICES after program start. "
+    "Setting the available devices to be zero."
+)
